206d19d30833b4a9e6cda2fa31eecc156924d981
蔡杰 <leo.cai@wichain.net>
2020-08-22 21:26:11 +0800

    add decorator pattern

---
+++ src/test/java/com/study/designPattern/designPattern/Coffee.java
@@ -0,0 +1,20 @@
+package com.study.designPattern.designPattern;
+
+import com.study.designPattern.decoratorPattern.BitterCoffee;
+import com.study.designPattern.decoratorPattern.CoffeeDecorator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Coffee {
+
+    @Test
+    public void addSugerIntoCoffee (){
+        BitterCoffee coffee = new BitterCoffee();// 点了一杯苦咖啡
+        coffee = new CoffeeDecorator(coffee); // 给咖啡加了点糖
+        coffee.printMaterial(); // 糖 咖啡
+    }
+}
